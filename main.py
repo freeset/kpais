be9afb369b45dca66c780b4d20dcbe073595731c
Peter Demeter <peter.demeter98@gmail.com>
2023-12-06 14:46:29 +0100

    testy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import copy
 import time
-import cProfile
-import pstats
-from io import StringIO
 
 
 class Node:
@@ -11,7 +8,6 @@ class Node:
         self.order = order
         self.row = row
         self.column = column
-
 
 def findSolution(startingRow, startingColumn, chessBoardSize, maxTime):
     # Vytvorenie sachovnice naplnenej nulami
@@ -26,54 +22,54 @@ def findSolution(startingRow, startingColumn, chessBoardSize, maxTime):
     allNodes = []
     allNodes.append(firstNode)
 
-    # Stav==1 prve riesenie sa naslo,start_time =spustenie casovaca
-    # numberOfGeneratedNodes nepotrebna premenna urcena iba na testovanie,predstavuje pocet
+    # Stav==1 prve riesenie sa naslo, start_time = spustenie casovaca
+    # numberOfGeneratedNodes nepotrebna premenna urcena iba na testovanie, predstavuje pocet
     # vygenerovanych uzlov v ramci behu programu
     stav = 0
     start_time = time.time()
     numberOfGeneratedNodes = 0
 
-    while (True):
+    while True:
         curent_time = time.time()
         # Max time je urceny ako 15 sekund
-        if (curent_time - start_time > maxTime):
+        if curent_time - start_time > maxTime:
             print("Velkost:", chessBoardSize, "Zaciatocny riadok:", startingRow + 1, "Zaciatocny stlpec:",
                   startingColumn + 1)
             print("Nenaslo sa riesenie v stanovenom case", "Pocet vygenerovanych uzlov:", numberOfGeneratedNodes)
             break
 
         # najdenie prveho riesenia
-        if (stav == 1):
+        if stav == 1:
             break
 
         # preskumanie vsetkych moznych uzlov
-        if (len(allNodes) == 0):
+        if len(allNodes) == 0:
             print("Velkost:", chessBoardSize, "Zaciatocny riadok:", startingRow + 1, "Zaciatocny stlpec:",
                   startingColumn + 1)
             print("Neexistuje riesenie", "Pocet vygenerovanych uzlov:", numberOfGeneratedNodes)
             break
 
-        # pop vrchneho uzla zo zasobnika  a prehladanie
+        # pop vrchneho uzla zo zasobnika a prehladanie
         lastNode = allNodes.pop()
         for i in range(0, 8):
-            # Posun z aktualnej pozicie na aktualnu poziciu+aktualny operand
+            # Posun z aktualnej pozicie na aktualnu poziciu + aktualny operand
             posRow = operands[i][0] + lastNode.row
             posCol = operands[i][1] + lastNode.column
 
             # Ak sa po posune nachadzame stale v sachovnici
-            if (posRow >= 0 and posRow <= chessBoardSize - 1) and (posCol >= 0 and posCol <= chessBoardSize - 1):
-                # AK na danom mieste este jazdec nebol
-                if (lastNode.chessboard[posRow][posCol] == 0):
+            if 0 <= posRow <= chessBoardSize - 1 and 0 <= posCol <= chessBoardSize - 1:
+                # Ak na danom mieste este jazdec nebol
+                if lastNode.chessboard[posRow][posCol] == 0:
                     # Vytvorime novy uzol s kopiou sachovnice jeho parenta
                     # Ale pridame na sucasnu poziciu tah kona
-                    # V ramci testovania si zapamatavam pocet vygenerovfanych uzlov
+                    # V ramci testovania si zapamatavam pocet vygenerovanych uzlov
                     newNode = Node(copy.deepcopy(lastNode.chessboard), lastNode.order, posRow, posCol)
                     newNode.order = lastNode.order + 1
                     newNode.chessboard[posRow][posCol] = newNode.order
-                    numberOfGeneratedNodes = numberOfGeneratedNodes + 1
-                    # Ak uz v  danom uzle bolo vykonanych n*n (n je velkost sachovnice)
-                    # tahov,tak jazdec presiel celu sachovnicu
-                    if (newNode.order >= chessBoardSize * chessBoardSize):
+                    numberOfGeneratedNodes += 1
+                    # Ak uz v danom uzle bolo vykonanych n*n (n je velkost sachovnice)
+                    # tahov, tak jazdec presiel celu sachovnicu
+                    if newNode.order >= chessBoardSize * chessBoardSize:
                         print("Velkost:", chessBoardSize, "Zaciatocny riadok:", startingRow + 1, "Zaciatocny stlpec:",
                               startingColumn + 1)
                         for j in range(chessBoardSize):
@@ -85,9 +81,12 @@ def findSolution(startingRow, startingColumn, chessBoardSize, maxTime):
                     allNodes.append(newNode)
 
 
+
 def createChessboard(chessboardSize):
-    chessboard = [[0] * chessboardSize for i in range(chessboardSize)]
+    chessboard = [[0] * chessboardSize for _ in range(chessboardSize)]
     return chessboard
+
+
 
 
 def profile_code():
@@ -103,16 +102,7 @@ def profile_code():
     findSolution(2, 1, 6, 15)
 
 def main():
-    profiler = cProfile.Profile()
-    profiler.enable()
     profile_code()
-    profiler.disable()
-    output_stream = StringIO()
-    stats = pstats.Stats(profiler, stream=output_stream).sort_stats('cumulative')
-    stats.print_stats()
-
-    with open('profile_results.txt', 'w') as f:
-        f.write(output_stream.getvalue())
 
 if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,43 +1,51 @@
 import unittest
 from io import StringIO
 from unittest.mock import patch
-from main import findSolution, createChessboard, Node, main, profile_code
+from main import findSolution, createChessboard
 
 
-class TestChessboardKnight(unittest.TestCase):
-
-    def setUp(self):
-        self.held_output = StringIO()
-
-    def tearDown(self):
-        self.held_output.close()
+class EulerovKonTest(unittest.TestCase):
 
     def test_create_chessboard(self):
-        chessboard_size = 5
-        expected_chessboard = [[0] * chessboard_size for _ in range(chessboard_size)]
-        self.assertEqual(createChessboard(chessboard_size), expected_chessboard)
+        chessboard_size_5 = 5
+        expected_chessboard_5 = [[0] * chessboard_size_5 for _ in range(chessboard_size_5)]
+        self.assertEqual(createChessboard(chessboard_size_5), expected_chessboard_5)
 
-    def test_find_solution(self):
-        starting_row, starting_column, chessboard_size, max_time = 2, 2, 5, 5
+        chessboard_size_6 = 6
+        expected_chessboard_6 = [[0] * chessboard_size_6 for _ in range(chessboard_size_6)]
+        self.assertEqual(createChessboard(chessboard_size_6), expected_chessboard_6)
+
+    def test_find_solution_outputs(self):
+        test_cases = [
+            (4, 0, 5, 10),
+            (2, 2, 5, 10),
+            (3, 2, 5, 15),
+            (2, 1, 6, 15),
+        ]
+
+        for starting_row, starting_column, chessboard_size, max_time in test_cases:
+            with self.subTest(starting_row=starting_row, starting_column=starting_column,
+                              chessboard_size=chessboard_size, max_time=max_time):
+                with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+                    findSolution(starting_row, starting_column, chessboard_size, max_time)
+                    output = mock_stdout.getvalue().strip()
+
+                expected_output = f"Velkost: {chessboard_size} Zaciatocny riadok: {starting_row + 1} Zaciatocny stlpec: {starting_column + 1}"
+                self.assertIn(expected_output, output)
+
+    def test_final_matrix(self):
+        specific_test_case = (4, 0, 5, 10)
+        expected_output_for_specific_case = [
+            [3, 16, 7, 22, 5],
+            [8, 21, 4, 17, 12],
+            [15, 2, 11, 6, 23],
+            [20, 9, 24, 13, 18],
+            [1, 14, 19, 10, 25]
+        ]
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            findSolution(starting_row, starting_column, chessboard_size, max_time)
+            findSolution(*specific_test_case)
             output = mock_stdout.getvalue().strip()
-        self.assertIn(f"Velkost: {chessboard_size} Zaciatocny riadok: {starting_row + 1} Zaciatocny stlpec: {starting_column + 1}", output)
 
-    def test_profile_code(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            profile_code()
-            output = mock_stdout.getvalue().strip()
-        # Ensure that the output contains expected information for each call to findSolution
-        self.assertIn("Velkost: 5 Zaciatocny riadok: 3 Zaciatocny stlpec: 3", output)
-        self.assertIn("Velkost: 6 Zaciatocny riadok: 6 Zaciatocny stlpec: 1", output)
-        # Add more assertions based on the expected output
-
-    def test_main(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            main()
-            output = mock_stdout.getvalue().strip()
-        # Add assertions based on the expected output
-
-if __name__ == '__main__':
-    unittest.main()
+        for row, expected_row in zip((output.split('\n')[1:-1]), expected_output_for_specific_case):
+            row_values = [int(value) for value in row.strip()[1:-1].split(',')]
+            self.assertListEqual(row_values, expected_row)
